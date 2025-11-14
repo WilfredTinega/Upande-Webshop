d2cd7657295f679eea9a2056c6f6210ccce9ab8c
wilfredtinega <tinegawilfred2@gmail.com>
2025-11-14 23:47:01 +0300

    few changes to make things work

--- upande_webshop/upande_webshop/doctype/website_item/website_item.py
+++ upande_webshop/upande_webshop/doctype/website_item/website_item.py
@@ -2,6 +2,7 @@ import json
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    # Type-only import, never executed at runtime
     from erpnext.stock.doctype.item.item import Item
 
 import frappe
@@ -21,12 +22,22 @@ from upande_webshop.upande_webshop.doctype.override_doctype.item_group import (
     get_parent_item_groups,
     invalidate_cache_for,
 )
-from erpnext.stock.doctype.item.item import Item
-from erpnext.utilities.product import get_price
+
+# ---- SAFE OPTIONAL ERPNext IMPORTS ----
+try:
+    from erpnext.stock.doctype.item.item import Item
+    from erpnext.utilities.product import get_price
+except ImportError:
+    Item = None
+    get_price = None
+# ----------------------------------------
+
 from upande_webshop.upande_webshop.shopping_cart.cart import get_party
 from upande_webshop.upande_webshop.variant_selector.item_variants_cache import (
     ItemVariantsCacheManager,
 )
+
+
 class WebsiteItem(WebsiteGenerator):
     website = frappe._dict(
         page_title_field="web_item_name",
@@ -37,15 +48,16 @@ class WebsiteItem(WebsiteGenerator):
 
     def autoname(self):
         from frappe.model.naming import get_default_naming_series, make_autoname
+
         naming_series = get_default_naming_series("Website Item")
         if not self.name and naming_series:
             self.name = make_autoname(naming_series, doc=self)
 
     def onload(self):
-        super().onload()  # <--- updated
+        super().onload()
 
     def validate(self):
-        super().validate()  # <--- updated
+        super().validate()
 
         if not self.item_code:
             frappe.throw(_("Item Code is required"), title=_("Mandatory"))
@@ -71,160 +83,144 @@ class WebsiteItem(WebsiteGenerator):
             self.old_website_item_groups = [x[0] for x in result]
 
     def on_trash(self):
-        super().on_trash()  # <--- updated
+        super().on_trash()
         delete_item_from_index(self)
         self.publish_unpublish_desk_item(publish=False)
 
+
 def invalidate_item_variants_cache_for_website(doc):
-	"""
-	Rebuild ItemVariantsCacheManager via Item or Website Item
+    item_code = None
+    is_web_item = doc.get("published_in_website") or doc.get("published")
 
-	Args:
-		doc (Item): item of which cache should be cleared
-	"""
-	item_code = None
-	is_web_item = doc.get("published_in_website") or doc.get("published")
+    if doc.has_variants and is_web_item:
+        item_code = doc.item_code
+    elif doc.variant_of and frappe.db.get_value(
+        "Item", doc.variant_of, "published_in_website"
+    ):
+        item_code = doc.variant_of
 
-	if doc.has_variants and is_web_item:
-		item_code = doc.item_code
-	elif doc.variant_of and frappe.db.get_value(
-		"Item", doc.variant_of, "published_in_website"
-	):
-		item_code = doc.variant_of
+    if not item_code:
+        return
 
-	if not item_code:
-		return
-
-	item_cache = ItemVariantsCacheManager(item_code)
-	item_cache.rebuild_cache()
+    item_cache = ItemVariantsCacheManager(item_code)
+    item_cache.rebuild_cache()
 
 
 def invalidate_cache_for_web_item(doc):
-	"""
-	Invalidate Website Item Group cache and rebuild ItemVariantsCacheManager
-	Args:
-		doc (Item): document against which cache should be cleared
-	"""
-	invalidate_cache_for(doc, doc.item_group)
+    invalidate_cache_for(doc, doc.item_group)
 
-	website_item_groups = list(
-		set(
-			(doc.get("old_website_item_groups") or [])
-			+ [
-				d.item_group
-				for d in doc.get({"doctype": "Website Item Group"})
-				if d.item_group
-			]
-		)
-	)
+    website_item_groups = list(
+        set(
+            (doc.get("old_website_item_groups") or [])
+            + [
+                d.item_group
+                for d in doc.get({"doctype": "Website Item Group"})
+                if d.item_group
+            ]
+        )
+    )
 
-	for item_group in website_item_groups:
-		invalidate_cache_for(doc, item_group)
+    for item_group in website_item_groups:
+        invalidate_cache_for(doc, item_group)
 
-	# Update Search Cache
-	update_index_for_item(doc)
-
-	invalidate_item_variants_cache_for_website(doc)
+    update_index_for_item(doc)
+    invalidate_item_variants_cache_for_website(doc)
 
 
 def on_doctype_update():
-	# since route is a Text column, it needs a length for indexing
-	frappe.db.add_index("Website Item", ["route(500)"])
+    frappe.db.add_index("Website Item", ["route(500)"])
 
 
 def check_if_user_is_customer(user=None):
-	from frappe.contacts.doctype.contact.contact import get_contact_name
+    from frappe.contacts.doctype.contact.contact import get_contact_name
 
-	if not user:
-		user = frappe.session.user
+    if not user:
+        user = frappe.session.user
 
-	contact_name = get_contact_name(user)
-	customer = None
+    contact_name = get_contact_name(user)
+    customer = None
 
-	if contact_name:
-		contact = frappe.get_doc("Contact", contact_name)
-		for link in contact.links:
-			if link.link_doctype == "Customer":
-				customer = link.link_name
-				break
+    if contact_name:
+        contact = frappe.get_doc("Contact", contact_name)
+        for link in contact.links:
+            if link.link_doctype == "Customer":
+                customer = link.link_name
+                break
 
-	return True if customer else False
+    return True if customer else False
 
 
 @frappe.whitelist()
 def make_website_item(doc, save=True):
-	"""
-	Make Website Item from Item. Used via Form UI or patch.
-	"""
-	if not doc:
-		return
+    if not doc:
+        return
 
-	if isinstance(doc, str):
-		doc = json.loads(doc)
+    if isinstance(doc, str):
+        doc = json.loads(doc)
 
-	if frappe.db.exists("Website Item", {"item_code": doc.get("item_code")}):
-		message = _("Website Item already exists against {0}").format(
-			frappe.bold(doc.get("item_code"))
-		)
-		frappe.throw(message, title=_("Already Published"))
+    if frappe.db.exists("Website Item", {"item_code": doc.get("item_code")}):
+        message = _("Website Item already exists against {0}").format(
+            frappe.bold(doc.get("item_code"))
+        )
+        frappe.throw(message, title=_("Already Published"))
 
-	website_item = frappe.new_doc("Website Item")
-	website_item.web_item_name = doc.get("item_name")
+    website_item = frappe.new_doc("Website Item")
+    website_item.web_item_name = doc.get("item_name")
 
-	fields_to_map = [
-		"item_code",
-		"item_name",
-		"item_group",
-		"stock_uom",
-		"brand",
-		"has_variants",
-		"variant_of",
-		"description",
-	]
-	for field in fields_to_map:
-		website_item.update({field: doc.get(field)})
+    fields_to_map = [
+        "item_code",
+        "item_name",
+        "item_group",
+        "stock_uom",
+        "brand",
+        "has_variants",
+        "variant_of",
+        "description",
+    ]
+    for field in fields_to_map:
+        website_item.update({field: doc.get(field)})
 
-	# Needed for publishing/mapping via Form UI only
-	if not frappe.flags.in_migrate and (
-		doc.get("image") and not website_item.website_image
-	):
-		website_item.website_image = doc.get("image")
+    if not frappe.flags.in_migrate and (
+        doc.get("image") and not website_item.website_image
+    ):
+        website_item.website_image = doc.get("image")
 
-	if not save:
-		return website_item
+    if not save:
+        return website_item
 
-	website_item.save()
+    website_item.save()
+    insert_item_to_index(website_item)
 
-	# Add to search cache
-	insert_item_to_index(website_item)
+    return [website_item.name, website_item.web_item_name]
 
-	return [website_item.name, website_item.web_item_name]
 
 @frappe.whitelist()
 def has_website_permission_for_website_item(doc, ptype, user, verbose=False):
-	# Check item group permissions for website
+    if user == "Administrator":
+        return True
 
-	if user == "Administrator":
-		return True
+    if frappe.has_permission("Website Item", ptype=ptype, doc=doc, user=user):
+        return True
 
-	if frappe.has_permission("Website Item", ptype=ptype, doc=doc, user=user):
-		return True
+    if not frappe.db.get_single_value(
+        "Webshop Settings", "login_required_to_view_products"
+    ):
+        return True
 
-	if not frappe.db.get_single_value("Webshop Settings", "login_required_to_view_products"):
-		return True
+    return False
 
-	return False
 
 @frappe.whitelist()
 def has_website_permission_for_item_group(doc, ptype, user, verbose=False):
-	# Check item group permissions for website
-	if user == "Administrator":
-		return True
+    if user == "Administrator":
+        return True
 
-	if frappe.has_permission("Item Group", ptype=ptype, doc=doc, user=user):
-		return True
+    if frappe.has_permission("Item Group", ptype=ptype, doc=doc, user=user):
+        return True
 
-	if not frappe.db.get_single_value("Webshop Settings", "login_required_to_view_products"):
-		return True
+    if not frappe.db.get_single_value(
+        "Webshop Settings", "login_required_to_view_products"
+    ):
+        return True
 
-	return False
+    return False
