a38e66850c868578630c1bb4feaddc2129c29d94
wilfredtinega <tinegawilfred2@gmail.com>
2025-11-15 18:44:16 +0300

    updated

--- upande_webshop/hooks.py
+++ upande_webshop/hooks.py
@@ -8,7 +8,7 @@ app_license = "mit"
 # Apps
 # ------------------
 
-required_apps = ["webshop"]
+# required_apps = ["erpnext","payments","webshop"]
 
 # Each item in the list will be shown as an app in the apps page
 add_to_apps_screen = [
@@ -16,7 +16,7 @@ add_to_apps_screen = [
 		"name": "upande_webshop",
 		"logo": "/assets/upande_webshop/images/UpandeLogo.png",
 		"title": "Upande Webshop",
-		"route": "/upande_webshop",
+		"route": "/webshop",
 		# "has_permission": "upande_webshop.api.permission.has_app_permission"
 	}
 ]
