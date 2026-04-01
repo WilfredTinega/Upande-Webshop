0c46f6f07fea97bff2fc937e856ce2ab809f4eb2
wilfredtinega <tinegawilfred2@gmail.com>
2026-04-01 17:05:46 +0300

    feat: added biometric logs doctype

---
+++ upande_webshop/upande_webshop/doctype/biometric_logs/biometric_logs.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Upande LTD and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Biometric Logs", {
+// 	refresh(frm) {
+
+// 	},
+// });
